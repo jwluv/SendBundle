2e3abcad40d7079807491474bbd92fc5aaf87d79
EDU <jwluv1220@gmail.com>
2018-10-30 13:07:30 +0900

    Add Log in information

--- app/src/main/java/com/lyna/www/sendbundle/ExplicitActivity.java
+++ app/src/main/java/com/lyna/www/sendbundle/ExplicitActivity.java
@@ -4,9 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class ExplicitActivity extends AppCompatActivity {
 
     TextView textViewExplicitBundle;
+    TextView textViewExplicitId, textViewExplicitPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +17,11 @@ public class ExplicitActivity extends AppCompatActivity {
 
         Bundle bundle = getIntent().getExtras();
         textViewExplicitBundle = (TextView)findViewById(R.id.textViewExplicitBundle);
+        textViewExplicitId = (TextView)findViewById(R.id.textViewExplicitId);
+        textViewExplicitPass = (TextView)findViewById(R.id.textViewExplicitPass);
+
         textViewExplicitBundle.setText(bundle.getString("singleName", "No Data"));
+        textViewExplicitId.setText(bundle.getString("login_id", "No Data"));
+        textViewExplicitPass.setText(bundle.getString("login_pass", "No Data"));
     }
 }
--- app/src/main/java/com/lyna/www/sendbundle/MainActivity.java
+++ app/src/main/java/com/lyna/www/sendbundle/MainActivity.java
@@ -5,10 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    Button buttonSendBundle;
+    Button buttonSendBundle, buttonLogin;
+    EditText editTextId, editTextPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,14 +18,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         buttonSendBundle = (Button)findViewById(R.id.buttonSendBundle);
         buttonSendBundle.setOnClickListener(this);
+
+        buttonLogin = (Button)findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener(this);
+
+        editTextId = (EditText)findViewById(R.id.editTextId);
+        editTextPass = (EditText)findViewById(R.id.editTextPass);
     }
 
     @Override
     public void onClick(View v) {
-        String text = ((Button)v).getText().toString();
+
         Bundle bundle = new Bundle();
-        bundle.putString("singleName", text);
         Intent intent = new Intent(this, ExplicitActivity.class);
+        switch(v.getId()) {
+
+            case R.id.buttonSendBundle:
+                String text = ((Button) v).getText().toString();
+                bundle.putString("singleName", text);
+                break;
+            case R.id.buttonLogin:
+                bundle.putString("login_id", editTextId.getText().toString());
+                bundle.putString("login_pass", editTextPass.getText().toString());
+                break;
+        }
         intent.putExtras(bundle);
         startActivity(intent);
     }
